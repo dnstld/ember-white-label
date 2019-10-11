3003c147970080f0391baae2252054bce9bed9ba
Denis Toledo <contato@denistoledo.com.br>
2019-10-11 17:42:02 -0300

    Adds build config

--- config/environment.js
+++ config/environment.js
@@ -5,6 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ember-white-label',
     environment,
     rootURL: '/',
+    theme: process.env.theme,
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -24,11 +25,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.theme = 'light';
   }
 
   if (environment === 'test') {
